366c8ef7ba30213533936ec8c69e4db7e95b8ad2
Joshua Chan <josh.chan.99@gmail.com>
2026-01-28 11:41:50 -0500

    Initial commit

---
+++ src/content/global.ts
@@ -0,0 +1,11 @@
+export const email = 'joshua.chan@aspirare.ai'
+export const contactSales = 'contact arte now'
+export const navLinks = [
+  { url: '/#services', text: 'services' },
+  { url: '/#about', text: 'about' },
+  { url: '/#about', text: 'careers' },
+  { url: '/#news', text: 'news' },
+  { url: '/#contact', text: 'contact' }
+]
+export const toc = { url: '/terms-conditions', text: 'Terms & Conditions' }
+export const copyright = 'copyright arte 2026'
